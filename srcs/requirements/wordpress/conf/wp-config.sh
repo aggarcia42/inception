e4f47a773c839608fbd0e7f3d3da63608ca35666
aggarcia <agusgarciaibero@gmail.com>
2023-05-11 20:45:24 +0200

    Wordpress: prueba permisos.

--- srcs/requirements/wordpress/conf/wp-config.sh
+++ srcs/requirements/wordpress/conf/wp-config.sh
@@ -3,11 +3,11 @@
 if [ ! -d "/var/www/html/wordpress" ]
 then
         mkdir -p /var/www/html/wordpress
-        chown -R www-data:www-data /var/www/html/wordpress
         cd var/www/html/wordpress
         wp core download --allow-root
         wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
         wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$MYSQL_USER --admin_password=$MYSQL_PASSWORD --admin_email=test@test.com --allow-root
         wp theme install inspiro --activate --allow-root
+        chown www-data:www-data /var/www/html/wordpress/ -R
 fi
-exec php-fpm7.3 --nodaemonize
+exec php-fpm7.3 -F
